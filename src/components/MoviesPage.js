549cd117c082d0e39d81a61752c1220a1507ae83
Ashley Rarieya <ararieya@gmail.com>
2023-11-13 05:19:44 +0300

    Completed

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -3,10 +3,17 @@ import { Route } from "react-router-dom";
 import MoviesList from "./MoviesList";
 
 function MoviesPage({ movies }) {
+  const match = useRouteMatch();
+  console.log(match);
+
   return (
     <div>
       <MoviesList movies={movies} />
+      <Route path={`${match.url}/:movieId`}>
+        <MovieShow movies={movies} />
+      </Route>
     </div>
   );
 }
+
 export default MoviesPage;
